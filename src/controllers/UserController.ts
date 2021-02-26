12edf378febf1c8b71ec6d00b2092ec4956cbccb
Daniel Costa <daniel.costa@cartaovaibem.com.br>
2021-02-26 18:22:34 -0300

    Starting Project

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,30 @@
+import { Request, response, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
+
+class UserController {
+  async create (req: Request, res: Response) {
+    const { name, email } = req.body;
+    const userRepository = getCustomRepository(UsersRepository);
+
+    const userAlreadyExists = await userRepository.findOne({
+      email
+    });
+
+    if (userAlreadyExists) {
+      return res.status(400).json({
+        error: "User already Exists!"
+      })
+    }
+
+    const user = userRepository.create({
+      name, email
+    })
+
+    await userRepository.save(user);
+
+    return res.status(201).json(user);
+  }
+}
+
+export { UserController };
